ed84cc81724d88011a6c82108b98928ff1f612f4
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-11 20:07:12 +0100

    Added picture_url to Flats to display images by erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111163243) do
+ActiveRecord::Schema.define(version: 20171111180537) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171111163243) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "picture_url"
   end
 
 end
